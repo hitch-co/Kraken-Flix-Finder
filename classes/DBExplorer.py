1a249784305b63eff832cbf7030526c77cd77495
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-01-27 13:06:23 -0500

    initial commit: class for exploring .db file using sqlite

---
+++ classes/DBExplorer.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+class DBExplorer:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = self._connect_to_db(self.db_path)
+        self.cursor = self.conn.cursor()
+
+    def _connect_to_db(self, db_path):
+        conn = sqlite3.connect(db_path)
+        return conn
+
+    def load_sql_file_from_path(self, path):
+        with open(path, 'r') as f:
+            sql = f.read()
+
+        print("LOG: This is the SQL imported:")
+        print(sql)
+        return sql    
+
+    def query_sql(self, sql):
+        self.cursor.execute(sql)
+        rows = self.cursor.fetchall()
+        
+        print(f"Number of rows: {str(len(rows))}")
+        return rows
+                    
+    def get_all_tables(self):
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = self.cursor.fetchall()
+
+        print("LOG: These are the tables in the database:")
+        for table in tables:
+            print(table)
+        return tables
+    
+    def get_table_schema(self, table_name):
+        self.cursor.execute("PRAGMA table_info('"+table_name+"');")
+        columns = self.cursor.fetchall()
+        print("LOG: These are the columns in the table: "+table_name)
+        for column in columns:
+            print(column)
+        return columns
+
+    def get_table_rows(self, table_name):
+        self.cursor.execute("SELECT * FROM "+table_name+";")
+        rows = self.cursor.fetchall()
+        print("LOG: These are the rows in the table: "+table_name)
+        for row in rows:
+            print(row)
+        return rows
+
+    def close_connection(self):
+        self.conn.close()
+
+def main(db_path, query_path):
+    dbexplorer = DBExplorer(db_path=db_path)
+    sql = dbexplorer.load_sql_file_from_path(path=query_path)
+    results = dbexplorer.query_sql(sql=sql)
+    for i, row in enumerate(results):
+        if i < 5:
+            print(f"Row {i}: {row}")

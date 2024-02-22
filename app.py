a9c360c2c73084dc6c896bc0d4c3ff531337fd73
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-02-22 13:10:59 -0500

    added genres endpoint, adjusted payload key names

--- app.py
+++ app.py
@@ -25,6 +25,22 @@ class MyFlaskApp:
         def home():
             return render_template('index.html')
 
+        @self.app.route('/genres', methods=['GET'])
+        def genres():
+
+            ############################################################
+            # Should probably be moved to a separate function
+            dbexplorer = DBExplorer(db_path=self.db_path)
+            query_path = './sql/get_genres.sql'
+            query = dbexplorer.load_sql_file_from_path(path=query_path)
+            results = dbexplorer.query_sql(query)
+            jsonified_results = jsonify(results)
+
+            self.logger.info(f"Number of rows: {str(len(results))}")
+            self.logger.info(f"Top 5 rows: {results[:5]}")
+            self.logger.debug(f"JSONified results: {jsonified_results}")
+            return jsonified_results
+                        
         @self.app.route('/search', methods=['POST'])
         def search():
 
@@ -36,8 +52,8 @@ class MyFlaskApp:
 
             data = request.json
             params = {
-                'uinp_name': '%' + data.get('name') + '%',
-                'uinp_genre': data.get('genre'),
+                'uinp_movie_name': '%' + data.get('movie_name') + '%',
+                'uinp_genre_id': data.get('genre_id'),
                 'uinp_year_min': data.get('year_min', 0),
                 'uinp_year_max': data.get('year_max', 3000)
             }
